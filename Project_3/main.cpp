009cf4a6aa3d0975667a9b2459333b35bb42c7b7
mpityo <mpityo@gmail.com>
2023-08-02 22:43:18 -0400

    merge sort and comparison overload for EarthquakeData

--- Project_3/main.cpp
+++ Project_3/main.cpp
@@ -1,9 +1,22 @@
 #include "DataLoader.cpp"
+#include "mergesort.h"
+//#include <QApplication>
+//#include <QLabel>
 
 int main(int argc, char **argv) {
     
-    std::vector<EarthquakeData> earthquakeData = loadEarthquakeDataFromCSV("../data.csv");
+    std::vector<EarthquakeData> earthquakeData = loadEarthquakeDataFromCSV("./data.csv");
     cout << earthquakeData.size() << " records loaded." << endl;
 
+    mergeSort(earthquakeData);
+
     return 0;
+
+//    Qt testing
+//    QApplication app(argc, argv);
+//
+//    QLabel label("Hello, Qt Widgets!");
+//    label.show();
+//
+//    return app.exec();
 }
