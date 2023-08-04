e6d59563f81dfcb985447c347ce10fbacbb6422f
mpityo <mpityo@gmail.com>
2023-08-03 21:31:56 -0400

    formatting in comparators

--- Project_3/comparators.h
+++ Project_3/comparators.h
@@ -1,52 +1,56 @@
 bool compareByMagnitude(const EarthquakeData& a, const EarthquakeData& b, string comparison, bool equal) {
-    if (comparison == "less")
+    if (comparison == "less") {
         if (equal)
             return a.getMagnitude() <= b.getMagnitude();
         else
             return a.getMagnitude() < b.getMagnitude();
-    else
+    } else {
         if (equal)
             return a.getMagnitude() >= b.getMagnitude();
         else
             return a.getMagnitude() > b.getMagnitude();
+    }
 
 }
 
 bool compareByLatitude(const EarthquakeData& a, const EarthquakeData& b, string comparison, bool equal) {
-    if (comparison == "less")
+    if (comparison == "less") {
         if (equal)
             return a.getLatitude() <= b.getLatitude();
         else
             return a.getLatitude() < b.getLatitude();
-    else
-    if (equal)
-        return a.getLatitude() >= b.getLatitude();
-    else
-        return a.getLatitude() > b.getLatitude();
+    } else {
+        if (equal)
+            return a.getLatitude() >= b.getLatitude();
+        else
+            return a.getLatitude() > b.getLatitude();
+    }
 }
 
 bool compareByLongitude(const EarthquakeData& a, const EarthquakeData& b, string comparison, bool equal) {
-    if (comparison == "less")
+    if (comparison == "less") {
         if (equal)
             return a.getLongitude() <= b.getLongitude();
         else
             return a.getLongitude() < b.getLongitude();
-    else
-    if (equal)
-        return a.getLongitude() >= b.getLongitude();
-    else
-        return a.getLongitude() > b.getLongitude();
+    } else {
+        if (equal)
+            return a.getLongitude() >= b.getLongitude();
+        else
+            return a.getLongitude() > b.getLongitude();
+    }
 }
 
 bool compareByDate(const EarthquakeData& a, const EarthquakeData& b, string comparison, bool equal) {
-    if (comparison == "less")
+    if (comparison == "less") {
         if (equal)
             return a.getDateStr() <= b.getDateStr();
         else
             return a.getDateStr() < b.getDateStr();
-    else
-    if (equal)
-        return a.getDateStr() >= b.getDateStr();
-    else
-        return a.getDateStr() > b.getDateStr();
+    } else {
+        if (equal)
+            return a.getDateStr() >= b.getDateStr();
+        else
+            return a.getDateStr() > b.getDateStr();
+    }
 }
